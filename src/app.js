9670f39cf088979ead53ab0d741133e9fbc85d8b
Jaime Rieiro <jaimeribeiro99@gmail.com>
2020-06-17 16:44:21 -0300

    :bug: Fix:  Fixed routes and tested

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,22 +11,72 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repository);
+  response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    response.status(400).json({ error: "Repository not found." })
+  };
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+  repositories[repositoryIndex] = repository;
+  response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex < 0) {
+    response.status(400).json({ error: "Repository not found." })
+  };
+  repositories.splice(repositoryIndex, 1);
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const { id } = request.params;
+  //valor retornado -1
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    response.status(400).json({ error: "Repository not found." })
+  };
+
+  const { likes } = repositories[repositoryIndex];
+
+  let countLike = likes + 1;
+  const repository = {
+    id,
+    title: repositories[repositoryIndex].title,
+    url: repositories[repositoryIndex].url,
+    techs: repositories[repositoryIndex].techs,
+    likes: countLike,
+  }
+  repositories[repositoryIndex] = repository;
+  response.json(repository);
 });
 
 module.exports = app;
+
+// a rota de post com like deve ser lida novamente
